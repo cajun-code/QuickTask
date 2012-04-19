51f74fcb8b260f2aec813de58ef705f9efdb95bc
Allan Davis <cajun.code@gmail.com>
2012-04-19 13:04:46 -0400

    Initial Commit

---
+++ QuickCheck/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  QuickCheck
+//
+//  Created by Allan Davis on 4/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ QuickCheckTests/QuickCheckTests.h
@@ -0,0 +1,13 @@
+//
+//  QuickCheckTests.h
+//  QuickCheckTests
+//
+//  Created by Allan Davis on 4/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface QuickCheckTests : SenTestCase
+
+@end
